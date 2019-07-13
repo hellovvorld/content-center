fed551d583dd299e0edb734c5ec0f6efefcf510a
yuyongtao <smallbadoncelaugh@gmail.com>
2019-07-13 18:10:12 +0800

    modify files !

--- src/main/java/com/itmuch/contentcenter/ContentCenterApplication.java
+++ src/main/java/com/itmuch/contentcenter/ContentCenterApplication.java
@@ -2,6 +2,9 @@ package com.itmuch.contentcenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import tk.mybatis.spring.annotation.MapperScan;
 
 // 扫描mybatis哪些包里面的接口
@@ -13,5 +16,11 @@ public class ContentCenterApplication {
     public static void main(String[] args) {
         SpringApplication.run(ContentCenterApplication.class, args);
     }
-
+    // 在spring容器中，创建一个对象，类型RestTemplate；名称/ID是：restTemplate
+    // <bean id="restTemplate" class="xxx.RestTemplate"/>
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }

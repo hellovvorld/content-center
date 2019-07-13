73ca45838896f1316e97f655911476d8b6a20a31
yuyongtao <smallbadoncelaugh@gmail.com>
2019-07-13 22:19:44 +0800

    modify files !

---
+++ src/main/java/com/itmuch/contentcenter/configuration/GlobalFeignConfiguration.java
@@ -0,0 +1,15 @@
+package com.itmuch.contentcenter.configuration;
+
+
+
+/**
+ * feign的配置类
+ * 这个类别加@Configuration注解了，否则必须挪到@ComponentScan能扫描的包以外
+ */
+public class GlobalFeignConfiguration {
+    /*@Bean
+    public Logger.Level level(){
+        // 让feign打印所有请求的细节
+        return Logger.Level.FULL;
+    }*/
+}
